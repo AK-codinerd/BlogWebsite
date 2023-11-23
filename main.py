0595f5fa544b31163290bf52f9f0812a4a04949b
Ak-codinerd <mohdakramali545@gmail.com>
2023-11-24 00:24:22 +0530

    idk man

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class BlogPost(db.Model):
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.Sting(500), nullable=False)
 # Adding a parent class here for the comments for blogpost
     post_comments = relationship("Comment", back_populates="parent_post")
 
